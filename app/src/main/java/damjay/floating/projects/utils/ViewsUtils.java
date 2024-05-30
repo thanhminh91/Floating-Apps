7f6674e7a22090a5c0929d2140351a396640e01b
John Olowe <olowejohn.2006@gmail.com>
2024-05-30 08:42:29 +0100

    Moved getViewTouchListener() to ViewsUtils and accommodated touch sloppiness

--- app/src/main/java/damjay/floating/projects/utils/ViewsUtils.java
+++ app/src/main/java/damjay/floating/projects/utils/ViewsUtils.java
@@ -1,13 +1,46 @@
 package damjay.floating.projects.utils;
 
+import android.content.res.Resources;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.ScrollView;
+import android.view.WindowManager;
 
 public class ViewsUtils {
+    public static View.OnTouchListener getViewTouchListener(final View parentLayout, final WindowManager window, final WindowManager.LayoutParams params) {
+        return new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                TouchState touchState = TouchState.getInstance();
+                touchState.moveTolerance = (int) (12.5f * Resources.getSystem().getDisplayMetrics().density);
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        touchState.setInitialPosition(event.getRawX(), event.getRawY());
+                        touchState.setOriginalPosition(params.x, params.y);
+                        return true;
+                    case MotionEvent.ACTION_MOVE:
+                        touchState.setFinalPosition(event.getRawX(), event.getRawY());
+                        if (touchState.hasMoved()) {
+                            params.x = touchState.updatedPositionX();
+                            params.y = touchState.updatedPositionY();
+                            window.updateViewLayout(parentLayout, params);
+                            return true;
+                        }
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        if (!touchState.hasMoved()) {
+                            boolean result = view.callOnClick();
+                            return result;
+                        }
+
+                }
+                return false;
+            }
+        };
+    }
 
     public static void addTouchListener(View parentView, View.OnTouchListener listener, boolean applyToChildren, boolean recursive, Class... allowedClasses) {
+        // Check if absent if the last class in the array is null
         boolean checkAbsent = allowedClasses != null && allowedClasses.length > 0 && allowedClasses[allowedClasses.length - 1] == null;
         if (parentView instanceof ViewGroup && (applyToChildren || recursive)) {
             ViewGroup viewGroup = (ViewGroup) parentView;
